c0fcfae91ade6ef182c7326f8666e07668fa0e1d
Angela <angela@jnickel.com>
2017-03-24 14:59:42 -0300

    fixed saving html tag

--- form.php
+++ form.php
@@ -5,7 +5,8 @@ if(isset($_POST['pass']) && $_POST['pass'] === getenv('PASSWORD')) {
 	foreach ($langs as $l)
 	{
 		$langlink = 	filter_var( $_POST['lang-link-' . $l], FILTER_SANITIZE_STRING);
-		$info = 			$_POST['info-' . $l];
+		$info = 			str_replace('<var rv-html="dataen.info">',"",$_POST['info-' . $l]);
+		$info = 			str_replace("</var>","",$info);
 	
 		$data = json_encode([
 			'lang-link' => $langlink,
