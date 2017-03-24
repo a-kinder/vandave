faaa9d12ff1c72cd64cab4eb693527fcac5004a1
Angela <angela@jnickel.com>
2017-03-24 14:47:43 -0300

    added WYSIWYG

--- form.js
+++ form.js
@@ -1,4 +1,14 @@
+$('#info-fr').summernote({
+  fontNames: ['Rouge Script', 'Julius Sans One'],
+  fontNamesIgnoreCheck: ['Rouge Script', 'Julius Sans One'],
+  defaultFontName: 'Julius Sans One'
+});
+$('#info-en').summernote({
+  fontNames: ['Rouge Script', 'Julius Sans One'],
+  fontNamesIgnoreCheck: ['Rouge Script', 'Julius Sans One'],
+    defaultFontName: 'Julius Sans One'
 
+});
 var getjson = $.getJSON('en.json', function(jsonen) {
 	var getjson = $.getJSON('fr.json', function(jsonfr) {
 		rivets.bind($('body'), {datafr: jsonfr, dataen: jsonen})
--- form.php
+++ form.php
@@ -5,23 +5,11 @@ if(isset($_POST['pass']) && $_POST['pass'] === getenv('PASSWORD')) {
 	foreach ($langs as $l)
 	{
 		$langlink = 	filter_var( $_POST['lang-link-' . $l], FILTER_SANITIZE_STRING);
-		$title = 			filter_var( $_POST['title-' . $l], FILTER_SANITIZE_STRING);
-		$subtitle = 	filter_var( $_POST['subtitle-' . $l], FILTER_SANITIZE_STRING);
-		$add1 = 			filter_var( $_POST['add1-' . $l], FILTER_SANITIZE_STRING);
-		$add2 = 			filter_var( $_POST['add2-' . $l], FILTER_SANITIZE_STRING);
-		$add3 = 			filter_var( $_POST['add3-' . $l], FILTER_SANITIZE_STRING);
-		$date = 			filter_var( $_POST['date-' . $l], FILTER_SANITIZE_STRING);
-		$time = 			filter_var( $_POST['time-' . $l], FILTER_SANITIZE_STRING);
-
+		$info = 			$_POST['info-' . $l];
+	
 		$data = json_encode([
 			'lang-link' => $langlink,
-			'title' => $title,
-			'subtitle' => $subtitle,
-			'address-line-1' => $add1,
-			'address-line-2' => $add2,
-			'address-line-3' => $add3,
-			'date' => $date,
-			'time' => $time
+			'info' => $info
 			]);
 		$ret = file_put_contents($l . '.json', $data);
 		if($ret === false) {
