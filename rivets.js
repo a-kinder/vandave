43a658b29f4eaec16fe1f02a8823f7c6cce62be3
angela-rose <angelarkinder@gmail.com>
2017-03-23 23:03:24 -0300

    translation and styles added

--- rivets.js
+++ rivets.js
@@ -1,4 +1,29 @@
+function getParameterByName(name, url) {
+  if (!url) {
+    url = window.location.href;
+  }
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-var getjson = $.getJSON('en.json', function(json) {
-	rivets.bind($('#container'), {data: json})
+
+var lang = getParameterByName('lang');
+if (lang === null || lang === 'en')
+{
+  lang = 'en';
+  var linklang = 'fr'
+}
+else {
+  var linklang = 'en'
+}
+document.getElementById('langlink').setAttribute('href', '?lang='+linklang);
+document.getElementById("anrsvp").setAttribute('src', 'https://daveandvan.anrsvp.com/'+lang+'/');
+
+
+var getjson = $.getJSON(lang + '.json', function(json) {
+	rivets.bind($('body'), {data: json})
 });
